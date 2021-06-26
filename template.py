a82934339398122bd14bf5c66b2c767f57df7c88
sakuyuno <forestry.for@gmail.com>
2021-06-26 14:20:45 +0900

    インポートと標準入力のテンプレート作成

---
+++ template.py
@@ -0,0 +1,38 @@
+import math
+
+"""
+# 1行に1つの文字列の入力を取得し、出力する
+s = input()
+#print(str)
+"""
+
+"""
+# 1行に1つの整数の入力を取得し、整数として出力する
+i = int(input())
+#print(i)
+"""
+
+"""
+# 1行に複数の入力値を取得し、出力する
+s = input().split()
+#print(s)
+"""
+
+"""
+# 1行に複数の整数の入力を取得し、整数として出力する
+i = list(map(int, input().split()))
+#print(i)
+"""
+
+"""
+# 複数行に複数の入力値を取得し、出力する
+s = [input() for i in range(n)] #nは場合に応じて定数を入れる
+#print(s)
+"""
+
+"""
+# 最初に入力回数、次に入力値を取得し、出力する
+N = int(input())
+s = [input() for i in range(N)]
+#print(s)
+"""
